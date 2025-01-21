8e6c8c9efe40900a322243b7e39f28f7fa3304ed
0xNick <3832465+0xNikolas@users.noreply.github.com>
2025-01-21 21:14:26 +0200

    feat(TokenClaim.sol): add token claim contract

---
+++ src/TokenClaim.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract TokenClaim is Ownable {
+    /*//////////////////////////////////////////////////////////////
+                            LIBRARIES
+    //////////////////////////////////////////////////////////////*/
+
+    using SafeERC20 for IERC20;
+
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
+    IERC20 public immutable token; // Token to be claimed
+    address public immutable treasury; // Treasury multisig holding the tokens
+    bytes32 public merkleRoot; // Root of the Merkle Tree
+    mapping(address => bool) public hasClaimed; // Track claimed addresses
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event TokensClaimed(address indexed claimant, uint256 amount);
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    error InvalidInput(string _input);
+    error InvalidMerkleProof();
+    error AlreadyClaimed();
+
+    /*//////////////////////////////////////////////////////////////
+                             MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
+    /*//////////////////////////////////////////////////////////////
+                            CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    /**
+     * @dev Set the Merkle Root
+     * @param _merkleRoot The root of the Merkle Tree
+     */
+    function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
+        if (_merkleRoot == bytes32(0)) revert(InvalidInput("_merkleRoot"));
+
+        merkleRoot = _merkleRoot;
+    }
+
+    /**
+     * @dev Claim tokens
+     * @param _amount Amount of tokens to claim
+     * @param _merkleProof Merkle proof to validate the claim
+     */
+    function claim(uint256 _amount, bytes32[] calldata _merkleProof) external {
+        if (hasClaimed[msg.sender]) revert(AlreadyClaimed());
+        if (_amount == 0) revert(InvalidInput("_amount == 0"));
+
+        // Verify the Merkle proof
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(msg.sender, _amount))));
+
+        if (!MerkleProof.verify(_merkleProof, merkleRoot, leaf)) revert(InvalidMerkleProof());
+
+        hasClaimed[msg.sender] = true;
+
+        // Transfer tokens to the claimant
+        token.safeTransferFrom(treasury, msg.sender, _amount);
+
+        emit TokensClaimed(msg.sender, _amount);
+    }
+
+    function withdraw(address _to, IERC20 _asset) external onlyOwner {
+        uint256 balance = _asset.balanceOf(address(this));
+        if(balance == 0) revert(InvalidInput("no balance"));
+
+        _asset.safeTransfer(_to, balance);
+    }
+
+}
