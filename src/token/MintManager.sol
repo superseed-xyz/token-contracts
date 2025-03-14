ab5751c12922e7ec1bece0d357eb414e24e541cb
0xNick <3832465+0xNikolas@users.noreply.github.com>
2025-03-14 14:14:40 +0200

    refactor(MintManager.sol): refactor mint function declaration
    set modifiers in correct order

--- src/token/MintManager.sol
+++ src/token/MintManager.sol
@@ -5,7 +5,8 @@ pragma solidity 0.8.28;
  * @title Superseed MintManager
  * @notice This is a temporary contract.
  *         In the context of controlled token inflation, this makes the most sense because we have enough flexibility
- *         to upgrade the mint manager on the token contract when needed, and these actions will be managed by a DAO eventually.
+ *         to upgrade the mint manager on the token contract when needed, and these actions will be managed by a DAO
+ * eventually.
  *         It's the only address having access to the mint function on the token.
  *         Minting is currently disabled.
  */
@@ -13,7 +14,7 @@ contract MintManager {
     /**
      * @dev This function is disabled in this temporary contract.
      */
-    function mint(address, uint256) pure external {
+    function mint(address, uint256) external pure {
         revert("Minting is disabled");
     }
 }
